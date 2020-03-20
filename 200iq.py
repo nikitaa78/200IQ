4c0cfa54abc2ed0f93cee7349fbc8646eb993603
Nikita Ashok <nikitamars@gmail.com>
2020-03-19 18:34:04 -0700

    lmao

---
+++ 200iq.py
@@ -0,0 +1,33 @@
+class Card:
+	def __init__(self, suit, val):
+		self.suit = suit
+		self.value = val
+
+class Deck:
+	def __init__(self):
+		self.cards = []
+		self.build()
+
+	def build(self):
+		for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
+			for v in range(1, 14):
+				self.cards.append(Card(s, v))
+
+
+class Game:
+	def __init__(self, deck, players):
+		self.deck = Deck().cards
+
+	def deal(self, players, cards):
+		#assign equal number cards to players randomly
+
+
+
+
+class Player(self):
+	cards = []
+	team = []
+
+
+	def declare(self):
+		#
